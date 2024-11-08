b75696df749bb4d22807153e57defbd32d956df2
Deekshigowda08 <149522963+Deekshigowda08@users.noreply.github.com>
2024-11-08 11:53:33 +0530

    dvsnkjh

--- app/api/get/route.js
+++ app/api/get/route.js
@@ -4,5 +4,6 @@ import { cse } from "../list";
 export async function GET(){
     await mongoose.connect('mongodb+srv://Toconnect:deekshi@cluster0.xpni6pr.mongodb.net/timetable?retryWrites=true&w=majority&appName=Cluster0')
     const result=await cse.findOne({ name:"CSE" })
+    console.log(result);
     return NextResponse.json({result});
 }
--- app/customlayout.jsx
+++ app/customlayout.jsx
@@ -5,14 +5,8 @@ const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function CustomLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${montserrat.className} antialiased`}
-      >
         <div className="absolute w-[100%] top-0 z-50 bg-white">
           {children}
         </div>
-      </body>
-    </html>
   );
 }
